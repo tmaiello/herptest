6572741b628fedb9331fe040597598c661ebbc3c
Matt Baumaister <mbaumaister@ufl.edu>
2020-11-05 19:32:18 -0500

    Autopush based on summary.csv

--- herptest/canvas.py
+++ herptest/canvas.py
@@ -1,4 +1,5 @@
 from canvasapi import Canvas
+from csv import reader
 
 with open("API_URL.txt", 'r') as url:
     API_URL = url.readline().rstrip('\n')
@@ -12,6 +13,9 @@ with open("Course.txt", 'r') as code:
 with open("Assignment.txt", 'r') as ass:
     ass_name = ass.readline().rstrip('\n')
 
+with open("Results.txt", 'r') as result:
+    summary = result.readline().rstrip('\n')
+
 canvas = Canvas(API_URL, API_Key)
 course = canvas.get_course(code)
 students = []
@@ -19,15 +23,25 @@ students = []
 for student in course.get_users(enrollment_type='student'):
     students.append(student.name.split(' ') + [student.id])
 
+results = []
+with open(summary, 'r') as _summary:
+    csv_reader = reader(_summary)
+    header = next(csv_reader)
+    if header != None:
+        for row in csv_reader:
+            results.append(row)
+
+for res in results:
+    res[1] = res[1].split('_', 1)[0]
+
 for assignment in course.get_assignments():
     if(ass_name == assignment.name):
         for sub in assignment.get_submissions():
-            if(sub.user_id == students[0][2]):
-                print(sub.score)
-                score = input("Change Blanchard's score to: ")
-                sub.edit(
-                    submission = {
-                        'posted_grade' : score
-                    }
-                )
-                print(sub.score)
+            for res in results:
+                if(str(sub.user_id) == res[1]):
+                    print("Score of " + res[0] + ", ID: " + res[1] + " changed from " + str(sub.score) + " to " + str(float(res[2])) + ".")
+                    sub.edit(
+                        submission = {
+                            'posted_grade' : float(res[2])
+                        }
+                    )
