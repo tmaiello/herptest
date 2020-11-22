888dbdedea880457d239c7791670c1837ccd4c2d
tmaiello <tmaiello@ufl.edu>
2020-11-21 21:38:05 -0500

    completed moss class functionality

--- herptest/moss.py
+++ herptest/moss.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv
 
 class MossUtil:
-    def __init__(self, dotenv_path, language):
+    def __init__(self, dotenv_path):
         load_dotenv(dotenv_path)  # load token from .env file
         self.userid = os.getenv("USERID")
+        if self.userid == None:
+            raise ValueError("Non-existant Moss Token, try running --setupenv")
 
     # Returns mosspy object bound to userid and language
     # Language choices:
@@ -15,26 +17,56 @@ class MossUtil:
     # "mips", "prolog", "spice", "vb", "csharp",
     # "modula2", "a8086", "javascript", "plsql", "verilog"]
     def init_moss(self, language):
-        self.moss_obj = mosspy.Moss(self.userid, language)
+        language_list = ["c", "cc", "java", "ml", "pascal", "ada",
+        "lisp", "scheme", "haskell", "fortran",
+        "ascii", "vhdl", "perl", "matlab", "python",
+        "mips", "prolog", "spice", "vb", "csharp",
+        "modula2", "a8086", "javascript", "plsql", "verilog"]
+        for lang in language_list:
+            if lang == language.lower():
+                self.moss_obj = mosspy.Moss(self.userid, language)
+                return
+        raise ValueError("Language not valid choice of language from MOSS.")
+            
+
 
     # Adds files to be sent with submission
-    def add_files(self):
-        self.moss_obj.addBaseFile("submission/a01.py")
-        self.moss_obj.addBaseFile("submission/test_student.py")
+    def add_files(self, file_name_base, file_name_submissions):
+        
+        if(os.path.isdir(file_name_base)):
+            base_files = os.listdir(file_name_base)
+        else:
+            # error if not directory (can't read in base files)
+            raise ValueError("Base file is not a directory",file_name_base)
+        
+        for base in base_files:
+            self.moss_obj.addBaseFile(os.path.join(file_name_base,base))
+            # print(base)
 
-        # Submission Files
-        self.moss_obj.addFile("submission/a01-sample.py")
-        self.moss_obj.addFilesByWildcard("submission/a01-*.py")
+        if(os.path.isdir(file_name_submissions)):
+            student_files = os.listdir(file_name_submissions)
+        else:
+            # error if not directory (can't read in base files)
+            raise ValueError("Student submissions file is not a directory",file_name_submissions)
+
+        for student in student_files:
+            # self.moss_obj.addFile(student)
+            submission_files = os.listdir(os.path.join(file_name_submissions, student))
+            for submission in submission_files:
+                self.moss_obj.addFile(os.path.join(file_name_submissions,student,submission))
+                # print(os.path.join(student, submission))
+            # self.moss_obj.addFilesByWildcard("submission/a01-*.py")
 
     # Sends files via url created with moss_obj
     def send_files(self):
         self.url = self.moss_obj.send() # Submission Report URL
+        # self.url = self.moss_obj.send(lambda: print('*', end='', flush=True))
 
         print ("Report Url: " + self.url)
 
     def save_files(self):
         # Save report file
-        self.moss_obj.saveWebPage(self.url, "submission/report.html")
+        self.moss_obj.saveWebPage(self.url, "report.html")
 
         # Download whole report locally including code diff links
-        mosspy.download_report(self.url, "submission/report/", connections=8)
+        mosspy.download_report(self.url, ".", connections=8, log_level=10, on_read=lambda url:  print('*', end='', flush=True))
