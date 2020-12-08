2957e3deead60d29e75b9e577fe5e7ca80fad750
tmaiello <tmaiello@ufl.edu>
2020-12-08 02:44:05 -0500

    adding description to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='pengtest',
     version='0.9.9.5',
     packages=['pengtest',],
     url='https://github.com/tmaiello/herptest',
     license='GPL 3',
-    author='Emma Andrews, Gerard Avecilla, Matthew Baumeister, Tyler Maiello, Matt McDermott',
+    author='Emma Andrews, Gerard Avecilla, Matthew Baumaister, Tyler Maiello, Matt McDermott',
     author_email='jjb@eng.ufl.edu',
     description='Test suite tools for instructors',
     install_requires=[
